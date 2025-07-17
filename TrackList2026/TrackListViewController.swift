986abd93c5af2ca652e78312a75904b7a9d7b8ed
Yuri Luka <lukayurii.work@gmail.com>
2025-07-17 11:40:58 +0500

    logic

--- TrackList2026/TrackListViewController.swift
+++ TrackList2026/TrackListViewController.swift
@@ -14,7 +14,6 @@ class TrackListViewController: UITableViewController {
         super.viewDidLoad()
         tableView.rowHeight = 80
         navigationItem.leftBarButtonItem = editButtonItem
-
     }
 
     // MARK: - Table view data source
@@ -37,6 +36,19 @@ class TrackListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let track = trackList[indexPath.row]
         performSegue(withIdentifier: "showDetails", sender: track)
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let curentTrack = trackList.remove(at: sourceIndexPath.row)
+        trackList.insert(curentTrack, at: destinationIndexPath.row)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
